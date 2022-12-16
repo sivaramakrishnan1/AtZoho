39e61c1c7cba8669332d6bfa91461f12e743f822
sivaramakrishnan1 <sivageeven@gmail.com>
2022-12-16 09:17:45 +0530

    Day:13:@Z

--- Assesment 6 (Array)/Chess.java
+++ Assesment 6 (Array)/Chess.java
@@ -1,70 +1,68 @@
-class Chess
-{
-	static int[] board;
-	public static void main(String args[])
-	{
-		board = new int[5];	
-		
-		for(int i = 0 ; i < board.length ; i++) {
-			if(i % 2 == 0)
-			{
-				board[i] = i+1;
-			}
-			else
-			{
-				board[i] = i + (board.length / 2);
-			}
-		}
-		display(board);
-		
-		// queen(0);
-	}	
-	
-	public static void queen(int n)
-	{
-		if(n >= board.length) return;
-		int pos = isValidMove(board);
-		if(pos != -1)
-		{
-			System.out.println(pos + " " + n);
-			swap(board, pos);
-			queen(n);
-			return;
-		}
-		display(board);
-		queen(n+1);		
-	}
-	
-	public static void swap(int[] board, int pos)
-	{
-		int temp = board[pos];
-		board[pos] = board[(pos+1) % board.length];
-		board[(pos+1) % board.length] = temp;		
-	}
-	
-	public static int isValidMove(int[] board)
-	{
-		for(int i = 0 ; i < board.length -1 ; i++)
-		{
-			if(board[i] == board[i+1] + 1 || board[i] == board[i+1] - 1 )
-				return i;
-		}
-		
-		return -1;
-	}
+public class Chess {
+	final int N = 5;
 
-	public static void display(int[] board)
+	void printSolution(int board[][])
 	{
-		for(int i=0 ; i < board.length ; i++)
-		{
-			for(int j = 0 ; j < board.length ; j++)
-			{
-				if(board[j] - 1 == i)
-					System.out.print((i+1) + " ");
-				else 
-					System.out.print(". ");
-			}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++)
+				System.out.print(" " + board[i][j]
+								+ " ");
 			System.out.println();
 		}
-	}	
+	}
+
+	boolean isSafe(int board[][], int row, int col)
+	{
+		int i, j;
+
+		for (i = 0; i < col; i++)
+			if (board[row][i] == 1)
+				return false;
+
+		for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+			if (board[i][j] == 1)
+				return false;
+
+		for (i = row, j = col; j >= 0 && i < N; i++, j--)
+			if (board[i][j] == 1)
+				return false;
+
+		return true;
+	}
+
+	boolean solveNQUtil(int board[][], int col)
+	{
+		if (col >= N)
+			return true;
+		for (int i = 0; i < N; i++) {
+			if (isSafe(board, i, col)) {
+				board[i][col] = 1;
+
+				if (solveNQUtil(board, col + 1) == true)
+					return true;
+				board[i][col] = 0;
+			}
+		}
+
+		return false;
+	}
+	
+	boolean solveNQ()
+	{
+		int board[][] = { { 0, 0, 0, 0, 0 },{ 0, 0, 0, 0, 0 },{ 0, 0, 0, 0, 0 },{ 0, 0, 0, 0, 0 },{ 0, 0, 0, 0, 0 },};
+
+		if (solveNQUtil(board, 0) == false) {
+			System.out.print("Solution does not exist");
+			return false;
+		}
+
+		printSolution(board);
+		return true;
+	}
+
+	public static void main(String args[])
+	{
+		Chess Queen = new Chess();
+		Queen.solveNQ();
+	}
 }
--- temp.java
+++ temp.java
@@ -1,16 +1,40 @@
-// import java.util.Scanner;
 import java.util.*;
 
-class temp
-{
+class temp 
+{	
 	public static void main(String args[])
 	{
-		int a = 10;
+		Set<Integer> hs = new HashSet<>();
+		hs.add(9);		
+		hs.add(9);		
+		hs.add(8);		
+		hs.add(19);
+		hs.add(7);
+		System.out.println("From Hash Set \t\t" + hs);
 		
-		if(a % 2 == 0)
-		{
-			System.out.println("Even");
-		}
-		System.out.println("Number");
+		Set<Integer> lhs = new LinkedHashSet<>();
+		lhs.add(9);
+		lhs.add(8);	
+		lhs.add(9);			
+		lhs.add(19);
+		lhs.add(7);
+		System.out.println("From Linked Hash Set \t" + lhs);
+		
+		Set<Integer> ts = new TreeSet<>();
+		ts.add(9);		
+		ts.add(9);		
+		ts.add(8);		
+		ts.add(19);
+		ts.add(7);
+		System.out.println("From Tree Set \t\t" + ts);
 	}
 }
+
+
+
+/* 
+From Hash Set           [19, 7, 8, 9]
+From Linked Hash Set    [9, 8, 19, 7]
+From Tree Set           [7, 8, 9, 19]
+ 
+*/
