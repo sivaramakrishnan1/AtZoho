ee00f6dae526b83e51aed721f2011fa0e66e35c2
Sivarama Krishnan Venkatrayan <sivageeven@gmail.com>
2023-01-04 23:17:22 +0530

    Day:26:H

--- Task/Evaluation 17(Leetcode)/Roman.java
+++ Task/Evaluation 17(Leetcode)/Roman.java
@@ -14,38 +14,28 @@ class Roman
 	}
 	
 	public int romanToInt(String str) {
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
-		
-		int[] num = new int[str.length()];
-		
-		for(int i = 0 ; i < str.length() ; i++)
-			num[i] = map.get(str.charAt(i));
-		
-		int sum = num[0];
-		// MCMXCIV
-		//1000 100 1000 10 100 1 5
-		
-		for(int i = 1 ; i < str.length() ; i++) {
-			System.out.println(num[i+1] + " " + sum);
-
-			if(i == str.length() - 1)
-			{
-				if(num[i-1] < num[i]) sum -= num[i-1];
-				sum += num[i];
-				break;
-			}
-			if(num[i] < num[i+1])
-				sum -= num[i];
-			sum += num[i+1];	
+		int sum = 0, prev = 1001;		
+		for(int i = 0 ; i < str.length() ; i++) {
+			int val = get(str.charAt(i));
+			sum += val;
+			if(prev < val)
+				sum -= 2 * prev;
+			prev = val;
 		}
-		
 		return sum;
     }
+
+	public int get(char c)
+	{
+		switch(c){
+			case 'I' : return 1;
+			case 'V' : return 5;
+			case 'X' : return 10;
+			case 'L' : return 50;
+			case 'C' : return 100;
+			case 'D' : return 500;
+			case 'M' : return 1000;
+		}
+		return 0;
+	}
 }
